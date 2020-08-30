cbd95e50c2cd626aa3b33a45378a12b8c0692222
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2020-08-30 16:33:12 +0600

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -8,3 +8,17 @@ temp_c=$(cat temps/region_C)
 
 # Printing out the three variables
 echo "The three temperatures were $temp_a, $temp_b, and $temp_c"
+
+
+# An array with these variables as elements
+region_temps=($temp_b $temp_c)
+
+# Calling an external program to get average temperature
+average_temp=$(echo "scale=2; (${temp_b[0]} + ${temp_c[0]}) / 2" | bc)
+
+# Appending average temp to the array
+region_temps+=($average_temp)
+
+# Printing out the whole array
+echo ${region_temps[@]}
+
